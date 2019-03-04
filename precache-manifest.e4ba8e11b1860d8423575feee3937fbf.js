5b9b3ea39feeb7ddcae559147166cd05eef4fe73
Artem <gretskovart@gmail.com>
2019-03-04 17:08:49 +0400

    Updates

---
+++ precache-manifest.e4ba8e11b1860d8423575feee3937fbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "43ad90f98442de7eeeae",
+    "url": "/test-task-react-nyamushka/static/css/main.80f1ceff.chunk.css"
+  },
+  {
+    "revision": "43ad90f98442de7eeeae",
+    "url": "/test-task-react-nyamushka/static/js/main.43ad90f9.chunk.js"
+  },
+  {
+    "revision": "3ee05757bde8e0d535b7",
+    "url": "/test-task-react-nyamushka/static/js/runtime~main.3ee05757.js"
+  },
+  {
+    "revision": "a30ee72fd9d60e64a496",
+    "url": "/test-task-react-nyamushka/static/css/2.d3e0a313.chunk.css"
+  },
+  {
+    "revision": "a30ee72fd9d60e64a496",
+    "url": "/test-task-react-nyamushka/static/js/2.a30ee72f.chunk.js"
+  },
+  {
+    "revision": "284a8185c6eecdf0a67cb9786071777b",
+    "url": "/test-task-react-nyamushka/static/media/cat.284a8185.svg"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/test-task-react-nyamushka/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "a3f1c08e09cadf7facf6d5e303b127d7",
+    "url": "/test-task-react-nyamushka/static/media/background.a3f1c08e.svg"
+  },
+  {
+    "revision": "8c8fc480217bb7b29d39f3dc045da17b",
+    "url": "/test-task-react-nyamushka/index.html"
+  }
+];
